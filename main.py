9eafde476a5d3235a3688c29a06ee697f96a7d66
preetanshaagrawal <preetanshaagrawal05@gmail.com>
2026-05-26 02:48:55 +0530

    "added readfile and multiple file handling features."

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
-print("My md5sum")
+import hashlib
+
+def readFiles(names):
+    ans = []
+    for name in names:
+        with open(name, "r", encoding="utf-8") as f:
+            f_data = f.readlines()
+        data = "".join(f_data)
+        # Add name of file with res
+        res = hashlib.md5(data.encode("utf-8")).hexdigest()
+        ans.append(res)
+    return ans
+
+def handleMultipleFiles(filename):
+    names = filename.split(" ")
+    ans = readFiles(names)
+    return ans
+
+if __name__ == "__main__":
+    filename = input("Enter the name of the file : ")
+    res = handleMultipleFiles(filename)
+    print(f"MD5 : {res}")
